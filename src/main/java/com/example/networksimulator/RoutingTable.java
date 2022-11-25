888e2e4d5045ecb811afb393cbd44547203681ee
maysamabdallah <117880012+maysamabdallah@users.noreply.github.com>
2022-11-24 22:45:50 -0600

    Update RoutingTable.java

--- src/main/java/com/example/networksimulator/RoutingTable.java
+++ src/main/java/com/example/networksimulator/RoutingTable.java
@@ -3,4 +3,19 @@
 package com.example.networksimulator;
 
 public class RoutingTable {
+
+    //Instance variables
+    private String routerName;
+    private int routerNum;
+    private int cost;
+
+    //RoutingTable constructor
+
+    public RoutingTable(String routerName, int routerNum, int cost) {
+        this.routerName = routerName;
+        this.routerNum = routerNum;
+        this.cost = cost;
+    }
+
+
 }
