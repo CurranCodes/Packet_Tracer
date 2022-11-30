0bc22201681b0060e955e28b16ad932665c6e50e
CurranTF <fitzgeraldcurran1@gmail.com>
2022-11-30 17:45:29 -0600

    Added some comments to Router.java

--- src/main/java/com/example/networksimulator/Router.java
+++ src/main/java/com/example/networksimulator/Router.java
@@ -5,8 +5,16 @@ package com.example.networksimulator;
 
 import java.util.ArrayList;
 public class Router {
-    private String deviceName;
+
+    // a Router's unique identifier (e.g. R1, R2, R3, ..., Rn)
+    private final String deviceName;
+
+    // an ArrayList of Routers that represents the connections that an
+    // instance of Router has to other Routers
     private ArrayList<Router> neighbors;
+    /** NOTE: Because These are Router references we can traverse the graph of the network with the above structure**/
+
+    //an ArrayList of String arrays that contain a router name in index 0 and a cost in index 1
     private ArrayList<String[]> cost;
 
     Router(String deviceName, ArrayList<Router> neighbors, ArrayList<String[]> cost){
@@ -16,7 +24,7 @@ public class Router {
     }
 
     public void route(){
-
+        //TODO: construct a routing table dynamically given the values of our instance variables
     }
 
     public String getDeviceName() {
