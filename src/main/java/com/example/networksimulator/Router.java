d93e1ffffa2c147fccb882e61b61f8dfb11fae35
CurranTF <fitzgeraldcurran1@gmail.com>
2022-11-30 19:10:38 -0600

    Created first full version of Router.java

--- src/main/java/com/example/networksimulator/Router.java
+++ src/main/java/com/example/networksimulator/Router.java
@@ -4,6 +4,8 @@
 package com.example.networksimulator;
 
 import java.util.ArrayList;
+import java.util.Objects;
+
 public class Router {
 
     // a Router's unique identifier (e.g. R1, R2, R3, ..., Rn)
@@ -11,16 +13,16 @@ public class Router {
 
     // an ArrayList of Routers that represents the connections that an
     // instance of Router has to other Routers
-    private ArrayList<Router> neighbors;
-    /** NOTE: Because These are Router references we can traverse the graph of the network with the above structure**/
+    private final ArrayList<Router> neighbors;
+    /** NOTE: Because this contains Router references, we can traverse the graph of the network with the above structure **/
 
-    //an ArrayList of String arrays that contain a router name in index 0 and a cost in index 1
-    private ArrayList<String[]> cost;
+    //an ArrayList of String arrays that contain a router name in index 0 and a costList in index 1
+    private final ArrayList<String[]> costList;
 
-    Router(String deviceName, ArrayList<Router> neighbors, ArrayList<String[]> cost){
+    Router(String deviceName, ArrayList<Router> neighbors, ArrayList<String[]> costList){
         this.deviceName = deviceName;
         this.neighbors = neighbors;
-        this.cost = cost;
+        this.costList = costList;
     }
 
     public void route(){
@@ -29,5 +31,57 @@ public class Router {
 
     public String getDeviceName() {
         return deviceName;
+    }
+
+    // "Removes" a connection associated with a given destination device and returns true
+    // if the connection to said device was non-existent it returns false
+    public boolean removeConnection(String deviceName){
+        for (Router neighbor : neighbors){
+            if (neighbor.getDeviceName().equals(deviceName)) {
+                //removes connection
+                neighbors.removeIf(n -> (Objects.equals(n.getDeviceName(), deviceName)));
+
+                //removes cost entry
+                costList.removeIf(n -> (n[0].equals(deviceName)));
+
+                //removes the connection and cost entry in the previously connected router
+                neighbor.removeConnection(this.deviceName);
+
+                //informs caller of successful operation
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // "Connects" this router to another router if the connection does not already exist
+    // returns true if operation is successful
+    public boolean addConnection(Router r, int cost){
+        // if the connection does not exist already (prevents infinite loop)
+        if (!neighbors.contains(r)){
+            //add the router reference to neighbors
+            neighbors.add(r);
+
+            //add a connection to this Router in Router r
+            r.addConnection(this, cost);
+
+            //create and enter the costEntry into costList
+            String[] costEntry = {r.getDeviceName(), Integer.toString(cost)};
+            costList.add(costEntry);
+
+            //inform method caller of successful operation
+            return true;
+        }
+        return false;
+    }
+
+    // Returns true if a "connection" exists for a given device name
+    public boolean connectedTo(String deviceName){
+        for (Router neighbor : neighbors) {
+            if (neighbor.getDeviceName().equals(deviceName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
