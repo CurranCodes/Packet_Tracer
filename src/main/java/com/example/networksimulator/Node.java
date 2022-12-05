118294fc02e5ae7cac7ffda3052acbc6ce815bd3
CurranTF <fitzgeraldcurran1@gmail.com>
2022-12-04 18:56:10 -0600

    Changed the structure of node so that we can access each node with a unique class selector once the graph is drawn

--- src/main/java/com/example/networksimulator/Node.java
+++ src/main/java/com/example/networksimulator/Node.java
@@ -4,8 +4,8 @@ public class Node {
     private final String id;
     private final String group;
 
-    public Node(String id, String group){
+    public Node(String id){
         this.id = id;
-        this.group = group;
+        this.group = id;
     }
 }
